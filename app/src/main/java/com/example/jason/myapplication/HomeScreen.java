74ff740443beabd8c2df7b35d8abfe48a95a7d6d
Daniel <danielmaskell21@gmail.com>
2018-09-26 11:57:41 +1200

    -Made the Home Screen open on app launch -Added buttons (trees, cart, products) -Pressing tress button brings up list of trees

--- app/src/main/java/com/example/jason/myapplication/HomeScreen.java
+++ app/src/main/java/com/example/jason/myapplication/HomeScreen.java
@@ -1,13 +1,26 @@
 package com.example.jason.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+
+        final Button tree_button = findViewById(R.id.trees_button);
+        tree_button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(HomeScreen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 
 }
