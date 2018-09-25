fca80f4c4c9516d120eb5684343d8e2a18d51580
BigJase <38375797+BigJase@users.noreply.github.com>
2018-09-25 15:45:53 +1200

    Populated Recycler View
    Need to change Images and values

--- app/src/main/java/com/example/jason/myapplication/ProductAdapter.java
+++ app/src/main/java/com/example/jason/myapplication/ProductAdapter.java
@@ -5,46 +5,47 @@ package com.example.jason.myapplication;
 * RecyclerView.ViewHolder
 * */
 
-import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import java.util.List;
-
-public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder>{
-
-    private Context mCtx;
-    private List<Product> productList;
-
-    public ProductAdapter(Context mCtx, List<Product> productList) {
-        this.mCtx = mCtx;
-        this.productList = productList;
-    }
-
-    @NonNull
-    @Override
-    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(mCtx);
-        View view = inflater.inflate(R.layout.list_layout, null);
-        return new ProductViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ProductViewHolder holder, int i) {
-        Product product = new productList.get(i);
-
-       holder.textViewTitle.setText(product.getTitle());
-       holder.textViewDesc.setText(product.getShortdesc());
-       holder.textViewRating.setText(String.valueOf(product.getRating()));
-       holder.textViewPrice.setText(String.valueOf(product.getPrice()));
-
-    }
-
+//import android.content.Context;
+//import android.support.annotation.NonNull;
+//import android.support.v7.widget.RecyclerView;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//
+//import java.util.List;
+//
+//public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder>{
+//
+//    private Context mCtx;
+//
+//    private List<Product> productList;
+//
+//    public ProductAdapter(Context mCtx, List<Product> productList) {
+//        this.mCtx = mCtx;
+//        this.productList = productList;
+//    }
+//
+//
+//    @Override
+//    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        LayoutInflater inflater = LayoutInflater.from(mCtx);
+//        View view = inflater.inflate(R.layout.list_layout, null);
+//        return new ProductViewHolder(view);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull ProductViewHolder holder, int i) {
+//        Product product = new productList.get(i);
+//
+//       holder.textViewTitle.setText(product.getTitle());
+//       holder.textViewShortDesc.setText(product.getShortdesc());
+//       holder.textViewRating.setText(String.valueOf(product.getRating()));
+//       holder.textViewPrice.setText(String.valueOf(product.getPrice()));
+//
+//    }
+//
 //    @Override
 //    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
 //       Product product = new productList.get(position);
@@ -57,26 +58,110 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
 //       holder.imageView.setImageDrawable(mCtx.getResources().getDrawable(product.getImage()));
 //
 //    }
+//
+//
+//
+//
+//@Override
+//    public int getItemCount() {
+//        return productList.size();
+//    }
+//
+//    class ProductViewHolder extends RecyclerView.ViewHolder{
+//
+//        ImageView imageView;
+//        TextView textViewTitle, textViewShortDesc, textViewRating, textViewPrice;
+//
+//        public ProductViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//
+//            imageView = itemView.findViewById(R.id.imageView);
+//            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+//            textViewShortDesc = itemView.findViewById(R.id.textViewShortDesc);
+//            textViewRating = itemView.findViewById(R.id.textViewRating);
+//            textViewPrice = itemView.findViewById(R.id.textViewPrice);
+//        }
+//    }
+//
+//}
+
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import java.util.List;
+
+/**
+ * Created by Belal on 10/18/2017.
+ */
+
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+
+
+    //this context we will use to inflate the layout
+    private Context mCtx;
+
+    //we are storing all the products in a list
+    private List<Product> productList;
+
+    //getting the context and product list with constructor
+    public ProductAdapter(Context mCtx, List<Product> productList) {
+        this.mCtx = mCtx;
+        this.productList = productList;
+    }
+
+    @Override
+    public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //inflating and returning our view holder
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.list_layout, null);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ProductViewHolder holder, int position) {
+        //getting the product of the specified position
+        Product product = productList.get(position);
+
+        //binding the data with the viewholder views
+        holder.textViewTitle.setText(product.getTitle());
+        holder.textViewShortDesc.setText(product.getShortdesc());
+        holder.textViewRating.setText(String.valueOf(product.getRating()));
+        holder.textViewPrice.setText(String.valueOf(product.getPrice()));
+
+        holder.imageView.setImageDrawable(mCtx.getResources().getDrawable(product.getImage()));
+
+    }
+
 
     @Override
     public int getItemCount() {
         return productList.size();
     }
 
-    class ProductViewHolder extends RecyclerView.ViewHolder{
 
+    class ProductViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewTitle, textViewShortDesc, textViewRating, textViewPrice;
         ImageView imageView;
-        TextView textViewTitle, textViewDesc, textViewRating, textViewPrice;
 
-        public ProductViewHolder(@NonNull View itemView) {
+        public ProductViewHolder(View itemView) {
             super(itemView);
 
-            imageView = itemView.findViewById(R.id.imageView);
             textViewTitle = itemView.findViewById(R.id.textViewTitle);
-            textViewDesc = itemView.findViewById(R.id.textViewShortDesc);
+            textViewShortDesc = itemView.findViewById(R.id.textViewShortDesc);
             textViewRating = itemView.findViewById(R.id.textViewRating);
             textViewPrice = itemView.findViewById(R.id.textViewPrice);
+            imageView = itemView.findViewById(R.id.imageView);
         }
     }
-
 }
+
