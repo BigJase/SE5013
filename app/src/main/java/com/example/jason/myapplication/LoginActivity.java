ea0a0c585457ba36874791a268c5c0734cce9262
BigJase <38375797+BigJase@users.noreply.github.com>
2018-09-28 09:42:58 +1200

    Adding login function

--- app/src/main/java/com/example/jason/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/jason/myapplication/LoginActivity.java
@@ -26,7 +26,7 @@ public class LoginActivity extends Activity implements View.OnClickListener{
     @Override
     public void onClick(View v) {
         switch(v.getId()){
-            case R.id.bLogin;
+            case R.id.bLogin:
             break;
         }
     }
