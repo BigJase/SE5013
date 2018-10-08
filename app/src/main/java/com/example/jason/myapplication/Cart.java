cdf937087a1c4a54929b381ec994f7a64f6be5ed
Kelvin52 <suilamchi0709@yahoo.com.hk>
2018-10-08 14:53:44 +1300

    Created Cart Act

--- app/src/main/java/com/example/jason/myapplication/Cart.java
+++ app/src/main/java/com/example/jason/myapplication/Cart.java
@@ -3,11 +3,11 @@ package com.example.jason.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CheckOut extends AppCompatActivity {
+public class Cart extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_check_out);
+        setContentView(R.layout.activity_Cart);
     }
 }
