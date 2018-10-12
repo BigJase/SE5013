69c9ed888384e758c6c7e073f9b310a362ed2487
Kelvin52 <suilamchi0709@yahoo.com.hk>
2018-10-12 15:28:12 +1300

    Try Push after running in phone

--- app/src/main/java/com/example/jason/myapplication/ItemDetails.java
+++ app/src/main/java/com/example/jason/myapplication/ItemDetails.java
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.lang.String.valueOf;
 
 public class ItemDetails extends AppCompatActivity {
 
@@ -21,6 +26,8 @@ public class ItemDetails extends AppCompatActivity {
     Object item;
 
 
+
+
     CartItem cartItem;
 
     @Override
@@ -30,6 +37,7 @@ public class ItemDetails extends AppCompatActivity {
 
         final Button add_cart_button = findViewById(R.id.add_cart_button);
 
+        System.out.println("This is where the add to cart button is" );
         // This gets the current Cart Array
        // cartList = cart.getCart();
 
@@ -65,6 +73,9 @@ public class ItemDetails extends AppCompatActivity {
     }
 
     public void setObject(Object item){
+        System.out.println("setObject/ItemDetails");
+        System.out.println(itemName);
+        System.out.println(itemPrice);
         this.item = item;
     }
 
@@ -75,4 +86,8 @@ public class ItemDetails extends AppCompatActivity {
     public void setItem(CartItem item){
         this.cartItem = item;
     }
+
+
+//    This is item selection output
+
 }
